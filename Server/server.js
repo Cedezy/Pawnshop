c53d15c8e45d347e8c8d2063971e81bc42ce57bc
Cedezy <abcedeacalal18@gmail.com>
2025-12-26 17:16:37 +0800

    Adjust Server.js

--- Server/server.js
+++ Server/server.js
@@ -24,10 +24,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: "10mb" })); 
 app.use(cookieParser());
 app.use(cors({
-  origin: [
-    'http://localhost:5173'
-  ], 
-  credentials: true             
+  origin: process.env.CLIENT_URL,
+  credentials: true
 }));
 app.use((req, res, next) => {
   res.set('Cache-Control', 'no-store');
@@ -48,5 +46,5 @@ app.use('/api/rate', RateRoute);
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
